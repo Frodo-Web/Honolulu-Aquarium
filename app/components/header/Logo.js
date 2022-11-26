566c51cedc5b7bdfc9330ea8a52710a229d8e713
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-11-26 18:18:59 +0300

    Add navbar submenu

--- app/components/header/Logo.js
+++ app/components/header/Logo.js
@@ -1,15 +1,16 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import logo from './media/logo.png';
 import styles from '@styles/components/header/logo.module.scss';
 
 const Logo = async() => {
     return (
-        <div className={styles.logo}>
+        <Link className={styles.logo} href='/'>
             <Image
             src={logo}
             className={styles.image}
             />
-        </div>
+        </Link>
     )
 }
 
--- app/components/header/Navbar.js
+++ app/components/header/Navbar.js
@@ -1,36 +1,73 @@
 import Link from 'next/link';
 import styles from '@styles/components/header/navbar.module.scss';
+import localFont from '@next/font/local'
+
+const LEMONMILK = localFont({ src: '../../fonts/lemon_milk/LEMONMILK-Light.otf' })
 
 const Navbar = async () => {
     return (
         <nav className={styles.navbar}>
             <ul className={styles.menu}>
-                <li>
-                    <FirstLevelItem text='Plan a visit' to='/' />
-                    <ul></ul>
-                </li>
-                <li>
-                    <FirstLevelItem text='Experiences' to='/' />
-                    <ul></ul>
-                </li>
-                <li>
-                    <FirstLevelItem text='Animals & Exhibits' to='/' />
-                    <ul></ul>
-                </li>
-                <li>
-                    <FirstLevelItem text='Conservation & Community' to='/' />
-                    <ul></ul>
-                </li>
-                <li>
-                    <FirstLevelItem text='About Us' to='/' />
-                    <ul></ul>
-                </li>
+                <FirstLevelItem>
+                    <Link href='/'>Plan a visit</Link>
+                    <SubMenu>
+                        <Link href='/'>Buy tickets</Link>
+                        <Link href='/'>Pricing</Link>
+                        <Link href='/'>Become a member</Link>
+                        <Link href='/'>FAQs</Link>
+                        <Link href='/'>Park map</Link>
+                        <Link href='/'>School groups</Link>
+                        <Link href='/'>Dine</Link>
+                        <Link href='/'>Shop</Link>
+                    </SubMenu>
+                </FirstLevelItem>
+                <FirstLevelItem>
+                    <Link href='/'>Experiences</Link>
+                    <SubMenu>
+                        <Link href='/'>Aquari-OM Yoga Classes</Link>
+                        <Link href='/'>Coral Spawning</Link>
+                        <Link href='/'>Ocean Aloha</Link>
+                        <Link href='/'>Ocean Education Classroom</Link>
+                        <Link href='/'>Shark Dive</Link>
+                    </SubMenu>
+                </FirstLevelItem>
+                <FirstLevelItem>
+                    <Link href='/'>Animals & Exhibits</Link>
+                    <SubMenu>
+                        <Link href='/'>Hawaiʻi’s Marine Life</Link>
+                        <Link href='/'>Exhibits</Link>
+                        <Link href='/'>Humpbacks of Hawaiʻi</Link>
+                        <Link href='/'>Live Cam + Gallery</Link>
+                    </SubMenu>
+                </FirstLevelItem>
+                <FirstLevelItem>
+                    <Link href='/'>Conservation & Community</Link>
+                    <SubMenu>
+                        <Link href='/'>Beach Cleanups</Link>
+                        <Link href='/'>We Live Ocean Aloha</Link>
+                        <Link href='/'>Events</Link>
+                        <Link href='/'>Protect our water</Link>
+                    </SubMenu>
+                </FirstLevelItem>
+                <FirstLevelItem>
+                    <Link href='/'>About Us</Link>
+                    <SubMenu>
+                        <Link href='/'>Our story</Link>
+                        <Link href='/'>Newsroom</Link>
+                        <Link href='/'>Blog</Link>
+                        <Link href='/'>Careers</Link>
+                        <Link href='/'>Contact Us</Link>
+                    </SubMenu>
+                </FirstLevelItem>
             </ul>
         </nav>
     )
 }
 export default Navbar;
 
-const FirstLevelItem = async ({ text, to }) => {
-    return <Link className={styles.firstLevelItem} href={to}>{text}</Link>
+const FirstLevelItem = async ({ children }) => {
+    return <li className={styles.firstLevelItem}>{children}</li>
+}
+const SubMenu = async ({ children }) => {
+    return <ul className={`${styles.subMenu} ${LEMONMILK.className}`}>{children}</ul>
 }
